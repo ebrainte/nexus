cbff2166da66798f2dddcf83afb5e70f661ddc7b
Ariel Sandor <ebrainte@gmail.com>
2015-06-22 18:56:21 -0300

    added mkdir data folder

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ rm -R sonatype-work/ #delete original folder
 echo "Copying Config files\n"
 mv nexus/conf/nexus.properties nexus/conf/nexus.properties.old
 wget https://raw.githubusercontent.com/ebrainte/nexus/master/nexus.properties -O nexus/conf/nexus.properties
-mv nexusData/conf/nexus.xml nexusData/conf/nexus.xml.bak
+mkdir -p nexusData/conf/
+#mv nexusData/conf/nexus.xml nexusData/conf/nexus.xml.bak
 wget https://raw.githubusercontent.com/ebrainte/nexus/master/nexus.xml -O nexusData/conf/nexus.xml
-mv nexusData/conf/security.xml nexusData/conf/security.xml.bak
+#mv nexusData/conf/security.xml nexusData/conf/security.xml.bak
 wget https://raw.githubusercontent.com/ebrainte/nexus/master/security.xml -O nexusData/conf/security.xml
 echo "Now you can start nexus by doing nexus/bin/nexus start"
