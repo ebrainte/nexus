6b09ce1fb74f6435867a988f33012883646f8b6b
Ariel Sandor <ebrainte@gmail.com>
2015-08-24 16:25:36 -0300

    Create cron.sh

---
+++ cron.sh
@@ -0,0 +1,2 @@
+date >> /home/despegar/csyncoutput.log
+/usr/bin/csync -v /home/despegar/nexusData/storage/releases/ sftp://despegar@nexusprod-00/home/despegar/nexusData/storage/releases/ >> /home/despegar/csyncoutput.log
